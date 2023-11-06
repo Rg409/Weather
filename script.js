dc98bd8a22b0b808b421709d14b76c0c7a5ad532
Roman-Gorshkov <romgorsh@gmail.com>
2023-11-06 07:17:48 +0100

    .

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-weather = 'http://dataservice.accuweather.com/locations/v1/countries/{regionCode}'
+weather = `http://api.weatherapi.com/v1/current.json?key=${weatherKey}&q=${location}`
+weatherKey = '3f2e4013f0714bb1a5b60854230611'
